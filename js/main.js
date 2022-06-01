b24c438eec62cdd813251526f6420264d053d979
plan-03 <PLAN-03@IEMCO-GARMENTS.COM>
2022-06-01 13:43:44 +0200

    update on code

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let mainRequest = new XMLHttpRequest();
   mainRequest.addEventListener("readystatechange", function() {
   if (mainRequest.readyState == 4 && mainRequest.status == 200) {
     locationData = JSON.parse(mainRequest.response);
-    display()
+    display();
   }
 })
 }())
@@ -25,31 +25,31 @@ mainRequest.send();
 
 function display() {
   document.getElementById("days").innerHTML = `
-  <div class="col-lg-4" id="firstDay">
+  <div class="col-md-4" id="firstDay">
             <div class="first-day day rounded-top">
               <p class="text-white-50 text-center p-2">${locationData.location.localtime}</p>
               <div class="content px-4 pb-4">
-                  <h5 class="countryCity text-white fw-normal">${locationData.location.name} / ${locationData.location.country}</h5>
+                  <h5 class="countryCity text-white fw-normal">${locationData.location.name} </h5>
                 <div class="deg d-flex mt-4">
                   <div class="deg-text">
-                  <h2 class="degHot text-white fw-bold">${locationData.location.lat}<sup>o</sup>C</h2>
-                  <h6 class="degHot text-white fw-bold">${locationData.location.lon}<sup>o</sup>C</h6>
+                  <h2 class="degHot text-white fw-bold">${locationData.current.temp_c}<sup>o</sup>C</h2>
                   </div>
                   <img src="${locationData.current.condition.icon}" class="img-deg ms-2" alt="">
                   </div>
                 <span class="stutsOfWeather text-main-color my-4 d-block">${locationData.current.condition.text}</span>
                 <div class="icons d-flex">
-                  <span class="me-4 text-white-50"><img class="me-2" src="images/icon-umberella.png" alt="">${locationData.current.vis_km}%</span>
-                  <span class="me-4 text-white-50"><img class="me-2" src="images/icon-wind.png" alt="">${locationData.current.wind_kph}km/h</span>
-                  <span class="me-4 text-white-50"><img class="me-2" src="images/icon-compass.png" alt="">${locationData.current.wind_dir}</span>
+                  <span class="me-4 text-white"><img class="me-2" src="images/icon-umberella.png" alt="">${locationData.current.vis_km}%</span>
+                  <span class="me-4 text-white"><img class="me-2" src="images/icon-wind.png" alt="">${locationData.current.wind_kph}km/h</span>
+                  <span class="me-4 text-white"><img class="me-2" src="images/icon-compass.png" alt="">${locationData.current.wind_dir}</span>
                 </div>
               </div>
             </div>
-          </div>`
+          </div>
+          `
   for(let i = 1; i < locationData.forecast.forecastday.length; i++)
   document.getElementById("days").innerHTML += `
-          <div class="col-lg-4">
-            <div class="third-day day rounded-top text-center">
+          <div class="col-md-4">
+            <div class="next-day day rounded-top text-center">
               <p class="text-white-50 p-2">${locationData.forecast.forecastday[i].date}</p>
               <div class="content p-4">
                 <div class="deg">
